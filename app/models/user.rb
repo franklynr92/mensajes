4ce590b37e853839f1a8c599ef5b3820813121a8
Franklyn Rodriguez <franklynr92@gmail.com>
2020-10-28 09:03:17 -0400

    creating query methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
     has_many :sender, class_name: "User", inverse_of: :sent_messages
 
+    def sent_messages
+        Messages.wehre("sender_id = ? OR recipient_id = ?", self.id, self.id)
+    end
 end
