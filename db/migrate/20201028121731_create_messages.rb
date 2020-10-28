7619fcfe90b1adfcd8685e1eadd24605c1f4cfbc
Franklyn Rodriguez <franklynr92@gmail.com>
2020-10-28 08:23:14 -0400

    creating migrations for models(user and message)

--- db/migrate/20201028121731_create_messages.rb
+++ db/migrate/20201028121731_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.integer :sender_id null: false
-      t.integer :recipent_id null: false
-      t.text :body null: false
+      t.integer :sender_id, null: false
+      t.integer :recipent_id, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
