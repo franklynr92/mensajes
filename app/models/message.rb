04dea840ba7286883df9e9eea65abaddc35d0778
Franklyn Rodriguez <franklynr92@gmail.com>
2020-10-28 09:52:49 -0400

    fixing typos of attributes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
-    belongs_to :receipient, class_name: "User", inverse_of; :received_messages
+    belongs_to :recipent, class_name: "User", inverse_of: :received_messages
     belongs_to :sender, class_name: "User", inverse_of: :sent_messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
-    has_many :sender, class_name: "User", inverse_of: :sent_messages
+    has_many :received_messages, class_name: "Message", foreign_key: "recipient_id"
 
     def sent_messages
         Messages.where("sender_id = ? OR recipient_id = ?", self.id, self.id)
