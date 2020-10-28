32851fcf5c03cf086348729a03ee9dc7aaacab22
Franklyn Rodriguez <franklynr92@gmail.com>
2020-10-28 09:02:01 -0400

    creating relationships for the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
+    has_many :sender, class_name: "User", inverse_of: :sent_messages
+
 end
